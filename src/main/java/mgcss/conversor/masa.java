39d0a45571bfed5ed174d5b86b28ccc09f2bc2c6
usuario <usuario>
2017-03-21 13:50:44 +0100

    Masa

--- src/main/java/mgcss/conversor/masa.java
+++ src/main/java/mgcss/conversor/masa.java
@@ -6,5 +6,33 @@
 package mgcss.conversor;
 
 public class masa {
-    // TODO resto de métodos
+    public static double koku(double n){
+        
+        return n*(6*(Math.pow(10, -3)));
+    }
+    
+    public static double kann(double n){
+        
+        return n*(3*(Math.pow(10, -1)));
+    }
+    
+    public static double kinn(double n){
+        
+        return n*2;
+    }
+    
+    public static double monnme(double n){
+        
+        return n*267;
+    }
+    
+    public static double tael(double n){
+        
+        return n*26;
+    }
+    
+    public static double kuPing(double n){
+        
+        return n*27;
+    }
 }
